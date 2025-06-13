7b4a690cd40e67e995fb94a36c839964f5554268
horadrim-dev <email.alexss@gmail.com>
2025-06-13 23:12:03 +0700

    fix date index

--- app/docs/search_indexes.py
+++ app/docs/search_indexes.py
@@ -6,7 +6,7 @@ class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     subname = indexes.CharField(model_attr='subname', null=True)
     number = indexes.CharField(model_attr='number', null=True)
-    date = indexes.DateTimeField(model_attr='date', null=True)
+    date = indexes.DateField(model_attr='date', null=True)
 
     def get_model(self):
         return Document
