632a784211a182411e63dcf73a280288003a4eab
horadrim-dev <email.alexss@gmail.com>
2025-06-16 09:43:43 +0700

    turn on medialer

--- app/medialer/cms_toolbars.py
+++ app/medialer/cms_toolbars.py
@@ -13,17 +13,17 @@ class MedialerToolbar(CMSToolbar):
 
 
         page_menu = self.toolbar.get_menu(PAGE_MENU_IDENTIFIER)
-        media_menu = self.toolbar.get_or_create_menu(
-            key='medialer_cms_integration',
-            verbose_name='Медиа', 
-            position=page_menu
-        )
-        media_menu .add_sideframe_item(
-            name='Альбомы',
-            url=admin_reverse('medialer_album_changelist')
-        )
-        self.toolbar.add_modal_button(
-            name='Фото в галерею', 
-            url=admin_reverse('medialer_albumpicture_add'),
-            # on_close=reverse('news:index')
-            )
+        # media_menu = self.toolbar.get_or_create_menu(
+        #     key='medialer_cms_integration',
+        #     verbose_name='Медиа', 
+        #     position=page_menu
+        # )
+        # media_menu .add_sideframe_item(
+        #     name='Альбомы',
+        #     url=admin_reverse('medialer_album_changelist')
+        # )
+        # self.toolbar.add_modal_button(
+        #     name='Фото в галерею', 
+        #     url=admin_reverse('medialer_albumpicture_add'),
+        #     # on_close=reverse('news:index')
+        #     )
