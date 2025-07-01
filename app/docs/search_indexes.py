73d922a1aa21332a106e6f97d7a4f6d9db14f927
horadrim-dev <email.alexss@gmail.com>
2025-07-01 16:46:50 +0700

    add full name for doc search

--- app/docs/search_indexes.py
+++ app/docs/search_indexes.py
@@ -4,6 +4,7 @@ from .models import Document, DocumentCategory
 
 class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    fullname = indexes.CharField(model_attr='full_name', null=True)
     subname = indexes.CharField(model_attr='subname', null=True)
     number = indexes.CharField(model_attr='number', null=True)
     date = indexes.DateField(model_attr='date', null=True)
